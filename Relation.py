8337aa63e41ffd47a8dee69614e7d4642587264b
daelmo <justallphine@googlemail.com>
2018-11-23 18:28:57 +0100

    added Relation

---
+++ Relation.py
@@ -0,0 +1,7 @@
+
+class Relation:
+
+    def __init__(self, primaryKeyTable, primaryKey, referencedTable):
+        self.primaryKeyTable = primaryKeyTable
+        self.primaryKey = primaryKey
+        self.referencedTable = referencedTable
