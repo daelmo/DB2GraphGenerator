ac91e3459d493a08fd71dbd1dc4d2ca558c82726
daelmo <justallphine@googlemail.com>
2019-01-08 18:06:14 +0100

    writing to edge list

--- create_graph.py
+++ create_graph.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 from DBConnector import DBConnector
 from graph_generator.TranslatorManager import TranslatorManager
+import networkx as nx
 
 if __name__ == '__main__':
     with DBConnector() as dbconnector:
         translatormanager = TranslatorManager(dbconnector)
         graph = translatormanager.translate()
+        nx.write_edgelist(graph, 'data/edge_list/graph.edgelist')
 
         print(graph.number_of_nodes())
