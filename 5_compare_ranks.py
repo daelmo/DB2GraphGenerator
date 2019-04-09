dab00611db06a02c212f89a4cf92bda6b547e354
daelmo <justallphine@googlemail.com>
2019-04-09 15:17:53 +0200

    fixed spearman

--- 5_compare_ranks.py
+++ 5_compare_ranks.py
@@ -2,14 +2,15 @@ import pandas as pd
 import networkx as nx
 import numpy as np
 
-movie_name = 'movies_2'
+
+movie_name = '200000004'
 path = 'data/4_filmlists/'
-version = '2'
+version = '4'
 
 
 def calcSpearman(rank_list1, rank_list2, sorted_list):
-    sorted_ranks1 = [rank_list1.index(movie) +1 for movie in sorted_movie_list]
-    sorted_ranks2 = [rank_list2.index(movie) +1 for movie in sorted_movie_list]
+    sorted_ranks1 = [rank_list1.index(movie) for movie in sorted_movie_list]
+    sorted_ranks2 = [rank_list2.index(movie) for movie in sorted_movie_list]
     spearman_table = pd.DataFrame({'list1': sorted_ranks1, 'list2': sorted_ranks2})
     spearman_table['diff'] = spearman_table['list1'] - spearman_table['list2']
     spearman_table['diff_square'] = spearman_table['diff'] * spearman_table['diff']
@@ -54,7 +55,7 @@ if __name__ == '__main__':
     print('\n Check: adjGraph - adjGraph')
     print(calcSpearman(adjacencyGraphArray, adjacencyGraphArray, sorted_movie_list))
 
-    graph = nx.read_edgelist('data/1_edge_list/kaggle.edgelist', create_using=nx.DiGraph)
+    graph = nx.read_edgelist('data/1_edge_list/kaggle_numbers.edgelist', create_using=nx.DiGraph)
     print('\n adjVector - random list')
     print(type(graph.nodes))
     print(calcSpearman(adjacencyVectorArray, list(graph.nodes), sorted_movie_list))
